891596bee12744e74b45e03ff868bc0caf2a3773
Alex <alexander.christianson@outlook.com>
2024-08-19 20:33:43 +0200

    Ändrade browserouter name

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="figtree-font">
-      <BrowserRouter>
+      <BrowserRouter basename="/dynamiska-sidan-demo">
       <Header/>
         <Routes>
           <Route path="/" element={<Home />}/>
